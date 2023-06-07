f88318fff1ceebfb3b4079310e2fd40783ce8ebf
JAYA DUBEY <jayadubey6402@gmail.com>
2023-06-07 15:13:18 +0530

    Update and rename findNthRootOfM.cpp

---
+++ binary search/findNthRootOfM.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int power(int mid, int n){
+  int res = 1;
+  for(int i = 1; i <= n; i++)
+  res = res * mid;
+  return res;
+}
+int NthRoot(int n, int m) {
+  int lo = 1, hi = m;
+  long double ep = 1e-16;
+  while((hi - lo) > ep){
+    int mid = (lo + hi)/2;
+    if(power(mid, n) < m)
+    lo = mid;
+    else
+    hi = mid;
+  }
+  return lo;
+}
+

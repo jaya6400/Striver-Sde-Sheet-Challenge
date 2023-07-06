014f92e46fdeed0bc01caa706b1973765dd54eae
JAYA DUBEY <jayadubey6402@gmail.com>
2023-07-06 23:00:57 +0530

    Update matrix-chain-multiplication.cpp

--- dp/matrix-chain-multiplication.cpp
+++ dp/matrix-chain-multiplication.cpp
@@ -32,8 +32,8 @@ int matrixMultiplication(vector<int> &arr, int N)
     //using bottum up(tabulation)
     for(int i = 1; i < N; i++)
     dp[i][i] = 0;
-    for(int i = N-1; i >= 1; i--)
-    for(int j = i+1; j < N; j++){
+    for(int i = N-1; i >= 1; i--) //as we are doing bottum up i is running from N-1 to 1
+    for(int j = i+1; j <= N-1; j++){ //j is always right of i
         int minians = INT_MAX;
       for (int k = i; k < j; k++) {
         int ans = arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][j];
@@ -41,7 +41,7 @@ int matrixMultiplication(vector<int> &arr, int N)
       }
       dp[i][j] = minians;
     }
-    return dp[1][N-1];
+    return dp[1][N-1]; //final answer gets stored at the last most index of the loop, i index ends at 1 and j index ends at N-1, hence dp[1][N-1] has the ans
 }
 
  int main(){

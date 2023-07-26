29f5b626793c6cc6d60258c25d04fe154d3f5e57
JAYA DUBEY <jayadubey6402@gmail.com>
2023-07-26 21:14:04 +0530

    Create fractional-knapsack.cpp

---
+++ greedy/fractional-knapsack.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h> 
+
+bool comp(pair<int, int> a, pair<int, int> b){
+double c1 = (double)a.second/(double)a.first;
+double c2 = (double)b.second/(double)b.first;
+
+return c1>c2;
+}
+//tc = o(nlogn + n) , o(nlogn) for sorting, o(n) for looping 
+double maximumValue (vector<pair<int, int>>& items, int n, int w)
+{
+  double maxval = 0.0;
+  int currWeight = 0;
+  //sorting the weight-values in decreasing order
+  sort(items.begin(), items.end(), comp);
+  for(int i = 0; i < n; i++){
+    if(currWeight+items[i].first <= w)
+        {
+            currWeight += items[i].first;
+            maxval += items[i].second;
+        }else{
+            //if the remaining weight is less than w
+            //take only the fraction of the left indexes
+            maxval += (items[i].second/(double)items[i].first)*(w-currWeight);
+            break;
+        }
+  }
+  return maxval;
+}
